82a48422830b3e9a575f7f0915859374638321ee
nikita02k <gritskevich-nikita@mail.ru>
2024-02-15 13:10:26 +0700

    one2 hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 #include <string>
 int main() {
 string name;
-std::cout << "your name:";
+std::cout << "your name:";//vvod
 std::cin >> name
-std::cout << "hello world from @" << name << endl;
+std::cout << "hello world from @" << name << endl;//vivod
 return 0;
 	}
