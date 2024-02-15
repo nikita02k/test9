0c2dff76b619a8fb91aaee44c34d3b646d118c04
nikita02k <gritskevich-nikita@mail.ru>
2024-02-15 13:03:23 +0700

    one1 hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
-using namespace std;
 int main() {
 string name;
-cout << "your name:";
-cin >> name
-cout << "hello world from @" << name << endl;
+std::cout << "your name:";
+std::cin >> name
+std::cout << "hello world from @" << name << endl;
 return 0;
 	}
